6c1e6942a920f5986a169d7feae0dad7d31c42cd
Robert Crupa <robertcrupa@outlook.com>
2024-06-18 11:35:11 +0100

    Cancelling long task example

--- Chapter_2.py
+++ Chapter_2.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from asyncio import CancelledError
 
 from utils import async_timed
 from utils import delay
@@ -24,8 +25,30 @@ async def concurrent_routines_2_10() -> None:
 
 
 @async_timed()
+async def cancell_routine_2_11() -> None:
+    long_task = asyncio.create_task(delay(10))
+
+    seconds_elapsed = 0
+
+    while not long_task.done():
+        print(f'Task not finished after {seconds_elapsed} seconds')
+
+        await asyncio.sleep(1)
+
+        seconds_elapsed += 1
+
+        if seconds_elapsed == 4:
+            long_task.cancel()
+
+    try:
+        await long_task
+    except CancelledError:
+        print(f'Task was cancelled after {seconds_elapsed} seconds')
+
+
+@async_timed()
 async def main():
 
-    await concurrent_routines_2_10()
+    await cancell_routine_2_11()
 
 asyncio.run(main())
