0da5b618b119707b07e9bad13996f98d5209cd0e
Robert Crupa <robertcrupa@outlook.com>
2024-06-18 10:49:17 +0100

    Added timer to measure async functions

---
+++ utils/async_timer.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import functools
+import time
+from typing import Any
+from typing import Callable
+
+
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f'Starting {func} with args {args} {kwargs}')
+            start = time.time()
+
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f'Finished {func} in {total:.4f} seconds')
+
+        return wrapped
+    return wrapper
